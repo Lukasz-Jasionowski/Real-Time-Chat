7b5c06ad5a953d9cbf29e6b84c959eb0b59f08d9
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-02 14:48:58 +0200

    Import and use MultiChatSocket, MultiChatWindow and useMultiChatLogic from React-Chat-Engine-Advanced

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,17 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-};
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
+}
+
 export default ChatsPage;
